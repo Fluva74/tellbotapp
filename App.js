28cd7371f6e4711e41bf4a2fe6a6d28e652b138d
fluva74 <fluvatech@gmail.com>
2023-04-10 20:35:06 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, View, TextInput, ActivityIndicator } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 import { Button, Input } from 'react-native-elements';
-import {API_KEY} from "@env"
+
+// import {API_KEY} from "@env"
 
 const CHATBOT_USER_ID = 'chatbot_user_id';
 
@@ -13,8 +14,40 @@ const systemMessage = {
   system: true,
 };
 
+const CustomInputToolbar = (props) => {
+  const [inputText, setInputText] = useState('');
+
+  return (
+    <View style={{ padding: 8 }}>
+      <TextInput
+        style={{ padding: 12, borderColor: '#ccc', borderWidth: 1, borderRadius: 4 }}
+        placeholder="Type your message here..."
+        onChangeText={(text) => setInputText(text)}
+        value={inputText}
+        onSubmitEditing={(event) => {
+          const newMessage = [{
+            _id: Math.random().toString(36).substring(7),
+            text: event.nativeEvent.text,
+            createdAt: new Date(),
+            user: {
+              _id: 1,
+              name: 'User',
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+          }];
+          props.onSend(newMessage);
+          setInputText('');
+        }}
+        blurOnSubmit
+        {...props}
+      />
+  </View>
+  );
+};
+
 const App = () => {
   const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setMessages([
@@ -34,46 +67,72 @@ const App = () => {
   const onSend = async (newMessages = []) => {
     console.log('newMessages:', newMessages);
     const userMessage = newMessages[0];
-
+    setIsLoading(true);
+  
     setMessages((prevMessages) =>
       GiftedChat.append(prevMessages, newMessages),
     );
-
-    const chatbotResponse = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${API_KEY}`,
+  
+    try {
+      const chatbotResponse = await fetch('https://api.openai.com/v1/chat/completions', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer <<<ENTER THE API KEY HERE>>>`,
+  },
+  body: JSON.stringify({
+    model: 'gpt-3.5-turbo',
+    messages: [ 
+      {
+        role: 'user',
+        content: userMessage.text,
       },
-      body: JSON.stringify({
-        prompt: `User: ${userMessage.text}\nChatGPT:`,
-        max_tokens: 50,
-        n: 1,
-        stop: '\n',
-      }),
-    });
-
-    const chatbotResponseJson = await chatbotResponse.json();
-    const chatbotResponseText = chatbotResponseJson.choices[0].text;
-
-    const chatbotMessage = {
-      _id: Math.random().toString(36).substring(7),
-      text: chatbotResponseText,
-      createdAt: new Date(),
-      user: {
-        _id: CHATBOT_USER_ID,
-        name: 'ChatGPT',
-        avatar: 'https://placeimg.com/140/140/any',
-      },
-    };
-
-    setMessages((prevMessages) =>
-      GiftedChat.append(prevMessages, [chatbotMessage]),
-    );
+    ],
+    max_tokens: 50,
+    n: 1,
+    stop: '\n',
+  }),
+});
+  
+      const chatbotResponseJson = await chatbotResponse.json();
+      setIsLoading(false);
+      console.log('chatbotResponseJson:', chatbotResponseJson);
+  
+      if (chatbotResponseJson.choices && chatbotResponseJson.choices.length > 0) {
+        const chatbotResponseText = chatbotResponseJson.choices[0].message.content;
+      
+        const chatbotMessage = {
+          _id: Math.random().toString(36).substring(7),
+          text: chatbotResponseText,
+          createdAt: new Date(),
+          user: {
+            _id: CHATBOT_USER_ID,
+            name: 'ChatGPT',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        };
+      
+        setMessages((prevMessages) =>
+          GiftedChat.append(prevMessages, [chatbotMessage]),
+        );
+      } else {
+        console.log('No choices found in the API response');
+      }
+      
+    } catch (error) {
+      setIsLoading(false);
+      console.error('Error fetching chatbot response:', error);
+    }
   };
+  
 
   return (
     <SafeAreaView style={styles.container}>
+       {isLoading && ( 
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    )}
       <GiftedChat
         messages={messages}
         onSend={(newMessages) => onSend(newMessages)}
@@ -89,48 +148,31 @@ const App = () => {
             buttonStyle={{ height: 48 }}
           />
         )}
-        renderComposer={(props) => (
-          <Input
-          multiline
-          placeholder="Type your message here..."
-          onChangeText={(text) => console.log(text)}
-          defaultValue={props.text}
-          containerStyle={{ paddingHorizontal: 8, marginBottom: 4 }}
-          inputContainer {...props}
-        />
-        
-        
-          )}
-          renderAvatar={null}
-          renderUsernameOnMessage={true}
-          scrollToBottom={true}
-          alwaysShowSend={true}
-          textInputProps={{
-            returnKeyType: 'send',
-            onSubmitEditing: (event) => {
-              const newMessage = [{
-                _id: Math.random().toString(36).substring(7),
-                text: event.nativeEvent.text,
-                createdAt: new Date(),
-                user: {
-                  _id: 1,
-                  name: 'User',
-                  avatar: 'https://placeimg.com/140/140/any',
-                },
-              }];
-              onSend(newMessage);
-            },
-          }}
-        />
+        renderInputToolbar={(toolbarProps) => (
+          <CustomInputToolbar onSend={(newMessages) => onSend(newMessages)} {...toolbarProps} />
+        )}
+        renderAvatar={null}
+        renderUsernameOnMessage={true}
+        scrollToBottom={true}
+        alwaysShowSend={true}
+      />
       </SafeAreaView>
       );
       };
       
       const styles = StyleSheet.create({
-      container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      },
+        container: {
+          flex: 1,
+          backgroundColor: '#fff',
+        },
+        loadingContainer: { // Add this block
+          position: 'absolute',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        },
       });
       
       export default App;
